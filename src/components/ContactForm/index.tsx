0e044e19a32f9939c75b2f801d9795c35426606f
Shelkonty <hdfghdfud@gmail.com>
2025-01-28 10:44:03 +0500

    123

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -31,7 +31,7 @@ const Contact = ({ t }: { t: TFunction }) => {
         };
 
         try {
-            const response = await fetch('http://localhost:5000/api/consultation', {
+            const response = await fetch('https://backend-eosin-beta.vercel.app/api/consultation', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
