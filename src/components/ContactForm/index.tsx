f69c1a8d51f2c47e68f383b2d3c7fa1a4e6cfdce
Shelkonty <hdfghdfud@gmail.com>
2025-02-03 11:41:35 +0500

    123

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -45,6 +45,7 @@ const Contact = ({ t }: { t: TFunction }) => {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json'
                 },
+                credentials: 'include',
                 body: JSON.stringify(formData)
             });
 
